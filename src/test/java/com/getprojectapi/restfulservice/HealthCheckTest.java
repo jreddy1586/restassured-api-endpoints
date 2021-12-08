b73815d4133b7a6b2fe68eaefa17c4c78997cdd7
jreddy1586 <95558343+jreddy1586@users.noreply.github.com>
2021-12-08 14:14:23 -0500

    updating tests which has no auth

--- src/test/java/com/getprojectapi/restfulservice/HealthCheckTest.java
+++ src/test/java/com/getprojectapi/restfulservice/HealthCheckTest.java
@@ -1,19 +1,68 @@
 package com.getprojectapi.restfulservice;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.testng.AssertJUnit.assertEquals;
+
+import java.util.ArrayList;
 
 import org.testng.annotations.Test;
 
+import io.restassured.response.Response;
+
 public class HealthCheckTest {
 	
-	@Test
-	public void healthCheckTest() {
-		given().
-		when().
-			get("").
-		then().
-			assertThat().
-			statusCode(200);
-	}
+	 
+    @Test
+    public void getPullsAndAssertListSize() {
+        String url = "https://api.github.com/repos/octocat/hello-world/pulls";
+        ArrayList list = given().when()
+                .get(url)
+                .then()
+                .statusCode(201)
+                .extract()
+                .as(ArrayList.class);
+        assertEquals(list.size(),30);
+    }
+
+ 
+
+   @Test
+    public void getStatusCodeFromRepoAssignees() {
+        String url = "https://api.github.com/repos/octocat/hello-world/assignees";
+        given().when()
+                .get(url)
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+
+    @Test
+    public void deleteRequiresAuth(){
+        String url = "https://api.github.com/repos/octocat/hello-world/issues/42/assignees";
+        given().when()
+                .get(url)
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .body("message", containsString("Not"));
+    }
+
+    @Test
+    public void postMarkdownStatusSuccess(){
+        String body = "{\"text\":\"text\"}";
+        String url = "https://api.github.com/markdown";
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(body)
+                .when()
+                .post(url)
+                .then()
+                .extract().response();
+        assertEquals(response.statusCode(),200);
+
+    }
 
 }
